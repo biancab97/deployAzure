377e6313a7b768bccafbd0762356547b58080b77
biancab97 <biancabreeman@gmail.com>
2023-11-12 10:02:47 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import dash
+from dash import html, dcc, Output, Input
+
+
+# Create the app and add the pages
+dash_app = dash.Dash(__name__, use_pages=True)
+
+# Set the layout of the entire app
+dash_app.layout = html.Div(
+    [
+        # main app framework
+        html.Div("Python Multipage App with Dash!", style={'fontSize': 50, 'textAlign': 'center'}),
+        html.Div([
+            dcc.Link(page['name'] + "  |  ", href=page['path'])
+            for page in dash.page_registry.values()
+        ]),
+        html.Hr(),
+
+        # content of each page
+        dash.page_container
+    ]
+)
+
+if __name__ == "__main__":
+    dash_app.run(debug=True)
